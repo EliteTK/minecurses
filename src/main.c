7953b585f1f22bda2e223de081d855c5b20cab03
EliteTK <tomasz.kramkowski@gmail.com>
2014-06-06 16:42:54 +0100

    Removed, now pointless, +8s from colour enum.

--- src/main.c
+++ src/main.c
@@ -8,23 +8,23 @@
 #define MINE_DENSITY 0.15
 
 typedef enum {
-    N1 = 1, // COLOR_BLUE + 8
+    N1 = 1, // COLOR_BLUE
     N2, // COLOR_GREEN
-    N3, // COLOR_RED + 8
+    N3, // COLOR_RED
     N4, // COLOR_BLUE
     N5, // COLOR_RED
     N6, // COLOR_CYAN
     N7, // COLOR_BLACK
-    N8, // COLOR_BLACK + 8
+    N8, // COLOR_BLACK
 
     VISIBLE, // COLOR_WHITE
-    HIDDEN, // COLOR_BLACK + 8
+    HIDDEN, // COLOR_BLACK
 
     FLAG, // COLOR_RED
     QUERY, // COLOR_BLACK
 
     MINE, // COLOR_BLACK
-    NMINE, // COLOR_RED + 8
+    NMINE, // COLOR_RED
 } Colorpairs;
 
 static bool failed = false;
